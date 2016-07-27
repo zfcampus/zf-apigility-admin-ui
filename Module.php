a6465e02f675b41f89c4b48e9e041572e9d64bf1
Matthew Weier O'Phinney <matthew@zend.com>
2016-07-27 09:55:01 -0500

    Added license docblock to Module class

--- Module.php
+++ Module.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @link      http://github.com/zfcampus/zf-apigility-admin-ui for the canonical source repository
+ * @copyright Copyright (c) 2013-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
 namespace ZF\Apigility\Admin\Ui;
 
 class Module
